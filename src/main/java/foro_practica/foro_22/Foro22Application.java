44a0853ae0995372a5417e69f48a46a2ae318884
Angel-ZER0 <cora.angelandres@gmail.com>
2024-09-10 22:10:15 -0600

    Clase que contiene el método main y ejecutará el proyecto

---
+++ src/main/java/foro_practica/foro_22/Foro22Application.java
@@ -0,0 +1,13 @@
+package foro_practica.foro_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foro22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Foro22Application.class, args);
+	}
+
+}
