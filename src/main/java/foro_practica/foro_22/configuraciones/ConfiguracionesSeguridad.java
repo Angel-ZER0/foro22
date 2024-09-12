ba0b4c71f2160e0759795797218728d405f95b76
Angel-ZER0 <cora.angelandres@gmail.com>
2024-09-11 18:01:13 -0600

    Configuración que se encarga de deshabilitar el csrgf de la página y dar acceso o denegarlo a ciertos endpoints según sea el caso

---
+++ src/main/java/foro_practica/foro_22/configuraciones/ConfiguracionesSeguridad.java
@@ -0,0 +1,58 @@
+package foro_practica.foro_22.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import foro_practica.foro_22.usuarios.Roles;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ConfiguracionesSeguridad {
+
+	
+	private final FiltroDeSeguridad filtro;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+	return httpSecurity
+	        .csrf(csrf -> csrf.disable())
+	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/usuario/**")
+	                .permitAll()
+	                .requestMatchers(HttpMethod.DELETE, "/ocultar-publicacion/**").hasRole(Roles.ADMIN.toString())
+	                .requestMatchers(HttpMethod.PATCH, "/reponer-publicacion/**").hasRole(Roles.ADMIN.toString())
+	                .anyRequest()
+	                .authenticated())
+	        .addFilterBefore(filtro, UsernamePasswordAuthenticationFilter.class)
+	        .build();
+
+	}
+	
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+    	
+        return authenticationConfiguration.getAuthenticationManager();
+        
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	
+        return new BCryptPasswordEncoder();
+        
+    }
+	
+}
