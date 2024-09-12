01f49af795ab4070250af7a314d7662407135b9b
Angel-ZER0 <cora.angelandres@gmail.com>
2024-09-11 21:53:50 -0600

    archivos creados por el IDE

---
+++ src/test/java/Foro_practica/Foro_22/Foro22ApplicationTests.java
@@ -0,0 +1,13 @@
+package Foro_practica.Foro_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Foro22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
